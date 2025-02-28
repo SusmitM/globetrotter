faddccb412c2dd12993b59ec3b471f4fece9dee1
Susmit Mukherjee <75859651+SusmitM@users.noreply.github.com>
2025-03-01 02:57:29 +0530

    Feat: DB Model and SignUp Route

---
+++ src/model/User.ts
@@ -0,0 +1,57 @@
+import mongoose, {Schema,Document} from "mongoose";
+
+
+
+export interface User extends Document{
+    username:string;
+    email:string;
+    password?:string;
+    verifyCode?:string;
+    verifyCodeExpiry?:Date;
+    isVerified:boolean;
+    invitedBy?: string;  
+    highScore: number;
+}
+
+
+
+const UserScheam: Schema<User>=new Schema({
+    username:{
+        type:String,
+        required:[true,"Username is required"],
+        trim:true,
+        unique:true,
+    },
+    email:{
+        type:String,
+        required:[true,"Email is required"],
+        unique:true,
+        match:[/^\S+@\S+\.\S+$/,"Please use a valid email address"],
+    },
+    password:{
+        type:String,
+    },
+    verifyCode:{
+        type:String,
+    },
+    verifyCodeExpiry:{
+        type:Date,
+    },
+    isVerified:{
+        type:Boolean,
+        default:false,
+    },
+    invitedBy:{
+        type:String,
+        required:false,
+    },
+      highScore:{
+        type:Number,
+        default:0,
+    }
+})
+
+const UserModel=(mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User",UserScheam)
+
+
+export default UserModel
